cd1f7e899a6af1cebbc0584e3d6831f2150d6f08
14261 <t#@vtsd.com>
2022-11-01 09:23:44 -0400

    Lab 05

--- Lab05vst.java
+++ Lab05vst.java
@@ -2,21 +2,35 @@
 // This is the student, starting version of the Lab05 assignment.
 
 
+
 import java.awt.*;
 import java.applet.*;
 
-
-public class Lab05vst extends Applet
-{
-    public void paint(Graphics g)
-    {
+public class Lab05vst extends Applet {
+    public void paint(Graphics g) {
         int width = 980;
         int height = 630;
         int x1 = 10;
         int y1 = 640;
         int x2 = 990;
         int y2 = 640;
-        g.drawRect(10,10,width,height);
+        g.drawRect(10, 10, width, height);
+
+        for (int k = 0; k < 965; k += 14) {
+            g.drawLine(x1 + k, y1, x2, y2 - k);
+            y2 += 5;
+        }
+
+        for (int k = 0; k < 965; k += 14) {
+            g.drawLine(x2 - k, y2, x1, y1 - k);
+            y2 = 640;
+            y1 += 5;
+        }
+
+
+
+
+        }
+
     }
 }
-
--- Temp.java
+++ Temp.java
@@ -1,2 +1,15 @@
+
 public class Temp {
+    public static void main(String[] args) {
+
+
+        for (int row = 1; row <= 5; row ++) {
+            for(int star = 0; star < row; star++) {
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+        }
+    }
 }
